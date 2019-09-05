9cff0a2e1aef7eac9717e0535cc0c4664f86dd21
nero <dezzyboy@tradebit.ca>
2019-09-05 15:04:00 -0400

    change network

--- src/networks.js
+++ src/networks.js
@@ -2,13 +2,13 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 exports.bitcoin = {
   messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'bc',
+  bech32: 'nc',
   bip32: {
     public: 0x0488b21e,
     private: 0x0488ade4,
   },
-  pubKeyHash: 0x00,
-  scriptHash: 0x05,
+  pubKeyHash: 0x35,
+  scriptHash: 0x14,
   wif: 0x80,
 };
 exports.regtest = {
